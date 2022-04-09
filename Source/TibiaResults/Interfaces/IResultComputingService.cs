74813541dce09044ac50555ee1eed336da1d24dd
Jan Walczyński <jan.walczynski@perfectio.pl>
2022-04-10 00:43:30 +0200

    Computing category results

---
+++ Source/TibiaResults/Interfaces/IResultComputingService.cs
@@ -0,0 +1,11 @@
+﻿using TibiaResults.Models;
+
+namespace TibiaResults.Interfaces
+{
+    internal interface IResultComputingService
+    {
+        CategoryResult ComputeCategoryResult(IEnumerable<HighscoreEntry>? oldHighscore, IEnumerable<HighscoreEntry>? newHighscore);
+
+        CategoryResult ComputeExperienceCategoryResult(IEnumerable<HighscoreEntry>? oldHighscore, IEnumerable<HighscoreEntry>? newHighscore, ILevelTracker levelTracker);
+    }
+}
